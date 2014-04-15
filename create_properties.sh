50ecaf663b860f3b33b1d439fc8147fbfbefdb7b
Luca Severini <lucaseverini@mac.com>
2014-04-15 12:35:39 -0700

    Small changes to shell script to create the property file BuildTimeStrings.properties.

--- create_properties.sh
+++ create_properties.sh
@@ -10,13 +10,21 @@
 
 PROPERTIES_FILE_PATH=./src/rope1401/Resources/BuildTimeStrings.properties
 
+if [[ "$(uname)" = "Darwin" ]]
+then
+	PLATFORM="Mac OS X"
+else
+	PLATFORM="Windows"
+fi
+
 # Get Java/JDK version
-JDK_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')" (Mac OS X)"
+JDK_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')" ("$PLATFORM")"
 
 # Get current date and time
 BUILD_TIME=`date +"%h-%d-%Y %H:%M"`
 
 echo "# This file is created anew at every build of ROPE" > $PROPERTIES_FILE_PATH
+echo "# Created on "`date` >> $PROPERTIES_FILE_PATH
 echo >> $PROPERTIES_FILE_PATH
 echo CompilerJDK=$JDK_VERSION >> $PROPERTIES_FILE_PATH
 echo CompilerTime=$BUILD_TIME >> $PROPERTIES_FILE_PATH
